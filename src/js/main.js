8c7add5d7b14c7d45b8890fd2d1042a4f61a3d7c
Daisigu <bogd228@mail.ru>
2020-02-28 13:13:01 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -12,17 +12,46 @@ $(document).ready(function(){
     });   
 
     $('.l-slider').slick({
-      autoplay: false,
-      dots: true,
+      autoplay: true,
       autoplaySpeed: 4000,
-      nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-      prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>'
-    });   
-
+      infinite: true,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
 
 
     
-    $('.girls-section-item-detail').click(function(event){
+    $('.boys-section-item-detail').click(function(event){
        $(this).toggleClass('active').next().slideToggle(300);
     });  
 
