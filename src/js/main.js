77d165a410bb283f6545cee23b1346f3fbd09890
Daisigu <bogd228@mail.ru>
2020-03-01 12:25:09 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -11,45 +11,7 @@ $(document).ready(function(){
       prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>'
     });   
 
-    $('.l-slider').slick({
-      autoplay: true,
-      autoplaySpeed: 4000,
-      infinite: true,
-      arrows: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-
-
+    
     
     $('.animators-section-item-detail').click(function(event){
        $(this).toggleClass('active').next().slideToggle(300);
