e72ab5f9b925f5f53a28f8f444ad106c1a45ddb5
Daisigu <bogd228@mail.ru>
2020-05-28 22:09:45 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,15 @@
-$(document).ready(function(){
-   $('.header__burger').click(function(event){
-    $('.header__burger,.main-header__list').toggleClass('active');
-   });
+$('.slider-nav').slick({
 
-
-   $('.slider').slick({
-      autoplay: false,
-      autoplaySpeed: 4000,
-      swipe: false,
-      nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-      prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>'
-    });   
-
-   $('.l-slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: false,
-    pauseOnFocus: true,
-    pauseOnDotsHover: false,
-    nextArrow: false,
-    prevArrow: false,
-      responsive: [
-        {
-          breakpoint: 660,
-          settings: {
-          
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-
-
-    
-    $('.animators-section-item-detail').click(function(event){
-       $(this).toggleClass('active').next().slideToggle(300);
-    });  
-
-      $("#menu").on("click","a", function (event) {
-         //отменяем стандартную обработку нажатия по ссылке
-         event.preventDefault();
-   
-         //забираем идентификатор бока с атрибута href
-         var id  = $(this).attr('href'),
-   
-         //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-         
-         //анимируем переход на расстояние - top за 1500 мс
-         $('body,html').animate({scrollTop: top}, 1000);
-
-   });
-
-$('.header__burger').click(function(event){
-   $('.header-menu').toggleClass('active');
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  asNavFor: '.slider-for',
 });
-
-
-
-
-
-
-
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
 });
