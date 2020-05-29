edde4f3fd094f33ff11e9af307c2009bb7d354ae
Daisigu <bogd228@mail.ru>
2020-05-29 12:33:57 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,15 @@ $(document).ready(function(){
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
+      adaptiveHeight: true
     });
     $('.slider-nav').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
       asNavFor: '.slider-for',
       centerMode: true,
-      focusOnSelect: true
+      focusOnSelect: true,
+      adaptiveHeight: true
     });
 
 });
