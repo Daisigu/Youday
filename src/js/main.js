c46b12ec2e89fc5fe8c5d7120a46e726afb1d794
Daisigu <bogd228@mail.ru>
2020-05-30 18:33:47 +0700

    123

--- src/js/main.js
+++ src/js/main.js
@@ -32,19 +32,19 @@ $(document).ready(function(){
       focusOnSelect: true,
       adaptiveHeight: true
     });
-    $('.slider-for-show').slick({
+    $('.show__slider-for').slick({
     
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
       adaptiveHeight: true
     });
-    $('.slider-nav-show').slick({
+    $('.show__slider-nav').slick({
       slidesToShow: 3,
       slidesToScroll: 1,
       nextArrow: ' <img class="show-next-button"src="./src/img/prev.png" alt="">',
       prevArrow:' <img class="show-prev-button" src="./src/img/prev.png" alt="">',
-      asNavFor: '.slider-for-show',
+      asNavFor: '.show__slider-for',
       centerMode: true,
       focusOnSelect: true,
       adaptiveHeight: true
